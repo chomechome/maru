d6e5f0eef60c4a790cd76f417b533fc16ed1e81a
Vladislav Blinov <cunningplan@yandex.ru>
2019-08-25 13:43:44 +0500

    Fix flake8 errors

--- maru/tagger/numerical.py
+++ maru/tagger/numerical.py
@@ -7,8 +7,8 @@ from maru.tag import Tag
 from maru.tagger.abstract import ITagger, Tagged
 from maru.types import Text, Indices
 
-_REGEX = re.compile(f'(?P<{NumericalForm.REAL}>\d+[.,]\d+$)|'
-                    f'(?P<{NumericalForm.INTEGER}>\d+$)')
+_REGEX = re.compile(rf'(?P<{NumericalForm.REAL}>\d+[.,]\d+$)|'
+                    rf'(?P<{NumericalForm.INTEGER}>\d+$)')
 
 _INTEGER = Tag(pos=PartOfSpeech.NUMERICAL, numform=NumericalForm.INTEGER)
 _REAL = Tag(pos=PartOfSpeech.NUMERICAL, numform=NumericalForm.REAL)
